489ac9751fb998b5abdd13f3e0c9f92464e4a50a
macbury <macbury@gmail.com>
2013-09-22 17:37:42 +0200

    animated water

--- Axe/src/com/macbury/fabula/screens/CubeMapScreen.java
+++ Axe/src/com/macbury/fabula/screens/CubeMapScreen.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.macbury.fabula.graphics.CubeMap;
 import com.macbury.fabula.manager.G;
@@ -19,16 +21,18 @@ public class CubeMapScreen extends BaseScreen {
   private Mesh cube;
   private PerspectiveCamera camera;
   private Mesh mesh;
-  private float         amplitudeWave   = 0.3f;
+  private float         amplitudeWave   = 0.1f;
   private float         angleWave       = 0.0f;
-  private float         angleWaveSpeed  = 1.0f;
+  private float         angleWaveSpeed  = 2.0f;
+  private Animation     animation;
   
   public CubeMapScreen(GameManager manager) {
     super(manager);
     
-    this.cubemap = new CubeMap("textures/skybox/day");
-    this.cube    = Shapes.genCube();
-    
+    this.cubemap   = new CubeMap("textures/skybox/day");
+    this.cube      = Shapes.genCube();
+    this.animation = new Animation(0.25f, G.db.getAtlas("liquid").findRegions("water"));
+    this.animation.setPlayMode(Animation.LOOP);
     TriangleGrid triangleGrid = new TriangleGrid(20, 20, true);
     triangleGrid.using(AttributeType.Position);
     triangleGrid.using(AttributeType.Normal);
@@ -40,17 +44,21 @@ public class CubeMapScreen extends BaseScreen {
       for (int z = 0; z < triangleGrid.getRows(); z++) {
         /* Top right Vertex */
         n1 = triangleGrid.addVertex(x+1f, 0, z);
+        triangleGrid.addUVMap(1, 0);
         triangleGrid.addNormal();
         /* top left Vertex */
         n2 = triangleGrid.addVertex(x, 0, z);
+        triangleGrid.addUVMap(1, 1);
         triangleGrid.addNormal();
         /* bottom Right Vertex */
         n3 = triangleGrid.addVertex(x+1f, 0, z+1f);
+        triangleGrid.addUVMap(0, 0);
         triangleGrid.addNormal();
         
         triangleGrid.addIndices(n1,n2,n3);
         /* Bottom left Vertex */
         n1 = triangleGrid.addVertex(x, 0, z+1f);
+        triangleGrid.addUVMap(1, 0);
         triangleGrid.addNormal();
         
         triangleGrid.addIndices(n3,n2,n1);
@@ -102,15 +110,22 @@ public class CubeMapScreen extends BaseScreen {
     Gdx.gl20.glActiveTexture(GL20.GL_TEXTURE0);
     Gdx.gl20.glBindTexture(GL20.GL_TEXTURE_CUBE_MAP, cubemap.getTextureId());
     
-    this.camera.update();
-    
+    Gdx.gl20.glActiveTexture(GL20.GL_TEXTURE1);
     final float dt = Gdx.graphics.getRawDeltaTime();
     
-    angleWave += dt * angleWaveSpeed;
+    angleWave += dt;
+    TextureRegion region = this.animation.getKeyFrame(angleWave);
+    
+    region.getTexture().bind(1);
+    this.camera.update();
+    
+    
     
     G.shaders.begin("water");
       G.shaders.setUniformMatrix("u_model_view", this.camera.combined);
       G.shaders.setUniformf("u_wave_data", angleWave, amplitudeWave);
+      G.shaders.setUniformi("u_texture", 1);
+      G.shaders.setUniformf("u_texture_cordinates", region.getU(), region.getV(), region.getU2(), region.getV2());
       G.shaders.setUniformf("u_camera_position", this.camera.position.x, this.camera.position.y, this.camera.position.z);
       mesh.render(G.shaders.getCurrent(), GL10.GL_TRIANGLES);
     G.shaders.end();
